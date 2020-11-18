5bc2283db5ed8aada24cc9e387d9983bcb768c93
nicohnavarro <nicohnavarro@gmail.com>
2020-11-18 12:48:07 -0300

    :building_construction: Add Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './components/layout/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { DetalleAlumnoComponent } from './components/alumno/detalle-alumno/detalle-alumno.component';
+import { ListadoAlumnoComponent } from './components/alumno/listado-alumno/listado-alumno.component';
+import { ListadoMateriaComponent } from './components/materia/listado-materia/listado-materia.component';
+import { DetalleMateriaComponent } from './components/materia/detalle-materia/detalle-materia.component';
+import { DetalleProfesorComponent } from './components/profesor/detalle-profesor/detalle-profesor.component';
+import { ListadoProfesorComponent } from './components/profesor/listado-profesor/listado-profesor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    DetalleAlumnoComponent,
+    ListadoAlumnoComponent,
+    ListadoMateriaComponent,
+    DetalleMateriaComponent,
+    DetalleProfesorComponent,
+    ListadoProfesorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/profesor/detalle-profesor/detalle-profesor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-profesor',
+  templateUrl: './detalle-profesor.component.html',
+  styleUrls: ['./detalle-profesor.component.css']
+})
+export class DetalleProfesorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
